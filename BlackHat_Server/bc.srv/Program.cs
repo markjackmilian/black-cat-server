6e36bb7273a609c7a32bd9f5497d6e5ec1f3e484
marco <emme.milani@hotmail.it>
2022-01-05 16:10:10 +0100

    Start on v2 Add exit, restart and mutex

--- BlackHat_Server/bc.srv/Program.cs
+++ BlackHat_Server/bc.srv/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace bc.srv
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mime;
+using System.Reflection;
+using System.Threading;
+
+namespace bc.srv
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            
+            // todo init st_client
+
+            using (var mutex = new Mutex(false, "todo"))
+            {
+                if (!mutex.WaitOne(0, false))
+                    return;
+
+
+                // start all
+            }
+
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Restart();
+        }
+
+        public static void Restart()
+        {
+            // todo add delay to process start using extternal process cmd
+            Process.Start(Assembly.GetExecutingAssembly().Location);
+            Exit(1);
+        }
+
+        public static void Exit(int code)
+        {
+            Environment.Exit(code);
         }
     }
 }
